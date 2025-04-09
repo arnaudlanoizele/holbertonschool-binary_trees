583062441b053336fc37d5745ad66f5c8dea04c3
arnaudlanoizele <10476@holbertonstudents.com>
2025-04-09 12:16:49 +0200

    14. Balance factor/15. Is full

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,26 +1,46 @@
 #include "binary_trees.h"
 
+
 /**
- * binary_tree_nodes - A function that count the nodes with at least 1 child
+ * binary_tree_h - A function that measures the height of a binary tree
  *
- * @tree: A pointer to the root node
+ * @tree: A pointer to the tree to measure
  *
- * Return: The number of nodes or 0 if tree is NULL
+ * Return: The height of the binary tree, 0 if NULL
  */
 
-size_t binary_tree_nodes(const binary_tree_t *tree)
+size_t binary_tree_h(const binary_tree_t *tree)
 {
-	size_t left = 0, right = 0;
+	size_t left_h, right_h;
 
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left != NULL || tree->right != NULL)
-	{
-		left = binary_tree_nodes(tree->left);
-		right = binary_tree_nodes(tree->right);
-		return (1 + left + right);
-	}
 
-	return (0);
+	left_h = binary_tree_h(tree->left);
+	right_h = binary_tree_h(tree->right);
+
+	return (1 + (left_h > right_h ? left_h : right_h));
+}
+
+
+/**
+ * binary_tree_balance - A function that measures the balance factor of a btree
+ *
+ * @tree: A pointer to the root of the tree
+ *
+ * Return: The balance of the tree or 0 if tree is NULL
+ */
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int left_h, right_h;
+
+	if (tree == NULL)
+		return (0);
+
+	left_h = binary_tree_h(tree->left);
+	right_h = binary_tree_h(tree->right);
+
+	return (left_h - right_h);
 }
